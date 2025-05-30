eedc8dc84ef5ee7b852337ff4bf0baf7d6f0e9b5
rabie01 <rabeea2100@yahoo.com>
2025-05-30 15:39:14 -0500

    Update variables.tf

--- DevOps Project-01/terraform/variables.tf
+++ DevOps Project-01/terraform/variables.tf
@@ -1,1 +1,29 @@
 
+variable "aws_region" {
+  default = "us-east-1"
+}
+
+variable "war_s3_bucket" {
+  default = "mybucett21000"
+}
+variable "war_s3_key" {
+  default = "artifacts/dptweb-1.0.war"
+}
+
+variable "instance_type" {
+  default = "t3.micro"
+}
+
+variable "key_name" {
+  description = "SSH key pair name"
+  default = "devops-projects"
+}
+
+variable "desired_capacity" {
+  default = 1
+}
+
+variable "ami_id" {
+  description = "Base AMI for EC2 (e.g., Amazon Linux 2)"
+  default = "ami-05a3e0187917e3e24"
+}
