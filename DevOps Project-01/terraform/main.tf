bd883ec50ac99fac819abc34e9ac0d38998123a4
rabie01 <rabeea2100@yahoo.com>
2025-06-01 15:32:00 -0500

    Update main.tf

--- DevOps Project-01/terraform/main.tf
+++ DevOps Project-01/terraform/main.tf
@@ -31,7 +31,11 @@ resource "aws_route_table" "public" {
 }
 
 resource "aws_route_table_association" "public_a" {
-  subnet_id      = [aws_subnet.public_a.id, aws_subnet.public_b.id]
+  subnet_id      = aws_subnet.public_a.id
+  route_table_id = aws_route_table.public.id
+}
+resource "aws_route_table_association" "public_b" {
+  subnet_id      = aws_subnet.public_b.id
   route_table_id = aws_route_table.public.id
 }
 
