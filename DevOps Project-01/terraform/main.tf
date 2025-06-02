f3f98c32ced816cbbb56b60ff6d6ec22d1e3bec1
rabie01 <rabeea2100@yahoo.com>
2025-06-01 19:02:06 -0500

    Update main.tf

--- DevOps Project-01/terraform/main.tf
+++ DevOps Project-01/terraform/main.tf
@@ -58,7 +58,7 @@ resource "aws_security_group" "web_sg" {
     from_port       = 8080
     to_port         = 8080
     protocol        = "tcp"
-    security_groups = [aws_security_group.web_sg.id]  # ALB's SG
+    self            = true
   }
 
   egress {
