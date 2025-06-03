44ca8d5118264d214fd31e34d834ffa05842f95a
rabie01 <rabeea2100@yahoo.com>
2025-06-03 17:29:34 -0500

    Update terraform.tf

--- DevOps Project-01/terraform/terraform.tf
+++ DevOps Project-01/terraform/terraform.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "mybucett21000"
+    bucket         = "mybuckett21000"
     key            = "terraform/project01/terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "terraform-lock"  # optional but recommended for state locking
+    use_lockfile = "terraform-lock"  # optional but recommended for state locking
   }
 }
